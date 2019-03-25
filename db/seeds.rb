c855c42d9db5cf73053a95bbb1edf25ff656ca76
wulman16 <will.ulman@gmail.com>
2019-03-25 13:39:33 -0400

    create basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.destroy_all
 Gif.destroy_all
 User.destroy_all
-Review.destroy_all
 
-gif1 = Gif.create(title: "test", url: "https://media.giphy.com/media/l3q2yYNt8DXoyKRdm/giphy.gif")
-user1 = User.create(name: "spike")
-review1 = Review.create(rating: 5, content: "This gif was amazing!", gif_id: gif1.id, user_id: user1.id)
+tyra = Gif.create(title: "tyra", url: "https://media.giphy.com/media/OpMAJdvTvXQYM/giphy.gif")
+mark = User.create(name: "mark")
+review1 = Review.create(rating: 5, content: "What a relatable gif! Exactly what I feel like when I'm programming.", gif_id: tyra.id, user_id: mark.id)
+
+trash = Gif.create(title: "trash", url: "https://66.media.tumblr.com/28698d657cc1c5563ca77c1f17287905/tumblr_pfkug1t59y1w2im81_250.gif")
+helen = User.create(name: "helen")
+review2 = Review.create(rating: 4, content: "How meta. Really makes you think.", gif_id: trash.id, user_id: helen.id)
+
+rage = Gif.create(title: "rage", url:"https://media2.giphy.com/media/amUVFzg1wNZKg/giphy.gif")
+review3 = Review.create(rating: 2, content: "Could use more character development. Why is he so angry?", gif_id: rage.id, user_id: mark.id)
+review4 = Review.create(rating: 5, content: "A fascinating meditation on the human condition.", gif_id: rage.id, user_id: helen.id)
+
+judy = Gif.create(title: "judy", url:"https://media.giphy.com/media/7yDthHaq2haXS/giphy.gif")
