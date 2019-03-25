1153726235ac7a252a8b361c15436fc35a6aafa5
spikeburton <ralphburton3@gmail.com>
2019-03-25 13:10:27 -0400

    add show action to controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::ReviewsController < ApplicationController
+  before_action :find_review, only: [:show]
+
   def index
     @reviews = Review.all
     render json: @reviews
+  end
+
+  def show
+    render json: @review
+  end
+
+  private
+
+  def find_review
+    @review = Review.all(params[:id])
   end
 end
