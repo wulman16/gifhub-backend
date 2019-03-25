988607d82e23d425faf8b7c024af48a432eff077
spikeburton <ralphburton3@gmail.com>
2019-03-25 13:06:36 -0400

    add show route for gifs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :gifs, only: [:index]
+      resources :gifs, only: [:index, :show]
       resources :reviews, only: [:index]
     end
   end
