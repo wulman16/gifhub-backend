15e12aaa2200ec8528100e3ff514d00333f12aa4
spikeburton <ralphburton3@gmail.com>
2019-03-25 13:06:16 -0400

    add show action

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::GifsController < ApplicationController
+  before_action :find_gif, only: [:show]
+
   def index
     @gifs = Gif.all
     render json: @gifs
+  end
+
+  def show
+    render json: @gif
+  end
+
+  private
+
+  def find_gif
+    @gif = Gif.find(params[:id])
   end
 end
