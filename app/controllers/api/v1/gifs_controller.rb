487568f58e2bc2401d6b9d21521cbe9d1768e3c3
spikeburton <ralphburton3@gmail.com>
2019-03-25 17:38:14 -0400

    add create action to controller and create route

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -10,7 +10,20 @@ class Api::V1::GifsController < ApplicationController
     render json: @gif.to_json(include: [:reviews]), status: :ok
   end
 
+  def create
+    @gif = Review.new(gif_params)
+    if @gif.save
+      render json: @gif, status: :accepted
+    else
+      render json: { errors: @gif.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   private
+
+  def gif_params
+    params.permit(:title, :url)
+  end
 
   def find_gif
     @gif = Gif.find(params[:id])
