61c1b10f0c26492b88fd07069410137c701bde84
spikeburton <ralphburton3@gmail.com>
2019-03-26 10:47:44 -0400

    add update action to reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReviewsController < ApplicationController
-  before_action :find_review, only: [:show]
+  before_action :find_review, only: [:update]
 
   # def index
   #   @reviews = Review.all
@@ -9,6 +9,14 @@ class Api::V1::ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     if @review.save
+      render json: @review, status: :created
+    else
+      render json: { errors: @review.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    if @review.update(review_params)
       render json: @review, status: :accepted
     else
       render json: { errors: @review.errors.full_messages }, status: :unprocessible_entity
