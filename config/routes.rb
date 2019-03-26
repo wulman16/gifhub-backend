34e8e38d560fe0f3c079d440479876f498d340a7
spikeburton <ralphburton3@gmail.com>
2019-03-26 10:48:29 -0400

    update routes for reviews to include update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :gifs, only: [:index, :show, :create]
-      resources :reviews, only: [:create]
+      resources :reviews, only: [:create, :update, :destroy]
       resources :users, only: [:create]
     end
   end
