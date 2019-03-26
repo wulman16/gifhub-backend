b098e8248af4c1f0c75013cb5f9af78839311e39
spikeburton <ralphburton3@gmail.com>
2019-03-26 09:45:39 -0400

    add gif_json method

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,4 +1,13 @@
 class Gif < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  def gif_json
+    {
+      id: self.id,
+      url: self.url,
+      title: self.title,
+      reviews: self.reviews
+    }
+  end
 end
