7ef0761ebd7d1250c69f8d06208876ddecb51ebd
spikeburton <ralphburton3@gmail.com>
2019-03-26 10:50:11 -0400

    fix status codes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :accepted
+      render json: @user, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
     end
