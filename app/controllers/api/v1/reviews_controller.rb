2fbcb28f71e8bbf72e8e1a3fe2e26a1304c31c3e
spikeburton <ralphburton3@gmail.com>
2019-03-28 10:35:07 -0400

    fixed JSON rendered for reviews to include all relavent data on action completion

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -7,13 +7,13 @@ class Api::V1::ReviewsController < ApplicationController
   # end
 
   def show
-    render json: @review, status: :ok
+    render json: @review.review_json, status: :ok
   end
 
   def create
     @review = Review.new(review_params)
     if @review.save
-      render json: @review, status: :created
+      render json: @review.review_json, status: :created
     else
       render json: { errors: @review.errors.full_messages }, status: :unprocessible_entity
     end
@@ -21,7 +21,7 @@ class Api::V1::ReviewsController < ApplicationController
 
   def update
     if @review.update(review_params)
-      render json: @review, status: :accepted
+      render json: @review.review_json, status: :accepted
     else
       render json: { errors: @review.errors.full_messages }, status: :unprocessible_entity
     end
